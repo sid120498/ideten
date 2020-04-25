9f686284ff40c8fff5b467d81adcfd8fb5f1bb97
siddharth.sing <siddharth.sing@media.net>
2020-04-25 22:47:48 +0530

    project intiliazed

---
+++ src/test/java/com/sam/ideten/IdetenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.ideten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdetenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
